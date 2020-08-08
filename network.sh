b474e3861f8a4994fdb07d14c6a44772ac4488b6
11chubby11 <jordanchubb@gmail.com>
2020-08-08 18:27:22 +1200

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,34 +1,25 @@
 #https://www.raspberrypi.org/documentation/configuration/wireless/access-point-bridged.md
-
-#sudo apt update
-#sudo apt upgrade
+sudo apt update
+sudo apt upgrade
 sudo apt install hostapd
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
-
 sudo bash -c 'cat > /etc/systemd/network/bridge-br0.netdev <<EOF
 [NetDev]
 Name=br0
 Kind=bridge
 EOF'
-
 sudo bash -c 'cat > /etc/systemd/network/br0-member-eth0.network <<EOF
 [Match]
 Name=eth0
 [Network]
 Bridge=br0
 EOF'
-
 sudo systemctl enable systemd-networkd
-
 sudo sed -i '1s/^/denyinterfaces wlan0 eth0\n/' /etc/dhcpcd.conf
-
 sudo echo interface br0 >> /etc/dhcpcd.conf
-
 sudo rfkill unblock wlan
-
 #https://w1.fi/cgit/hostap/plain/hostapd/hostapd.conf
-
 sudo bash -c 'cat > /etc/hostapd/hostapd.conf <<EOF
 country_code=NZ
 interface=wlan0
@@ -44,8 +35,5 @@ wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
 rsn_pairwise=CCMP
 EOF'
-
 sudo bash -c 'echo hdmi_force_hotplug=1 >> /boot/config.txt'
-#gpu_mem=256
-
 sudo systemctl reboot
