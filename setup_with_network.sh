3d1068d89b00eaf56df533a3e58f3d86910acc8b
11chubby11 <jordanchubb@gmail.com>
2020-09-10 16:29:28 +1200

    Update setup_with_network.sh

--- setup_with_network.sh
+++ setup_with_network.sh
@@ -2,6 +2,7 @@
 sudo apt update
 #sudo apt upgrade -y
 pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
+sudo systemctl enable --now vncserver-x11-serviced.service
 sudo apt install hostapd
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
@@ -36,7 +37,11 @@ wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
 rsn_pairwise=CCMP
 EOF'
-sudo bash -c 'echo hdmi_force_hotplug=1 >> /boot/config.txt'
+sudo bash -c 'cat  >> /boot/config.txt <<EOF
+hdmi_force_hotplug=1
+start_x=1
+gpu_mem=512
+EOF'
 sudo bash -c 'cat >> /etc/fstab <<EOF
 /dev/sda1 /home/pi/Desktop/usb ntfs defaults,auto,users,rw,nofail 0 0
 EOF'
