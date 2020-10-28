1e4e68b833881134aa6cb5e769bace574eaccd84
11chubby11 <jordanchubb@gmail.com>
2020-10-28 20:15:11 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #https://www.raspberrypi.org/documentation/configuration/wireless/access-point-bridged.md
 sudo apt update
 #sudo apt upgrade -y
+sudo apt install nginx
 pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
 sudo systemctl enable --now vncserver-x11-serviced.service
 sudo bash -c 'cat  >> /boot/config.txt <<EOF
